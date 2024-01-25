7749f26cfd4fea77214e8317a5f50e1d879ec457
hbksilver <37729528+hbksilver@users.noreply.github.com>
2024-01-25 14:15:21 -0600

    UseState Demo2(arrays)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Employees from './Employees';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <Employees/>
 );
 reportWebVitals();
