bef8ffb57a0c721ce385aca4110cc70aa91614e7
hbksilver <37729528+hbksilver@users.noreply.github.com>
2024-01-25 15:05:20 -0600

    Child to parent communication demo

--- src/Employee.js
+++ src/Employee.js
@@ -1,25 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-const Employee = ({employees}) => {
-        return (<>
-                <table style={{ width: '60%' }} className='table'>
-                        <thead className="thead-light">
-                                <tr>
-                              <th>EmpID</th>
-                               <th>Name</th>
-                               <th>Designation</th>
-                                </tr>
-                        </thead>
-                        <tbody>
-       {employees.map(employee => {
-       return (<tr  key={employee.empId}>
- <td>{employee.empId}</td>
-  <td>{employee.name}</td>
-   <td>{employee.designation}</td>
-                        </tr>)
-                                })
-                              }
-                        </tbody>
-                </table><br/><br/>
-        </>)
-}
-export default Employee;
+const Employee = ({employees,addEmployee}) => {
+        return (<>
+                <table style={{ width: '60%' }} className='table'>
+                        <thead>
+                                <tr>
+                          <th>EmpID</th>
+                           <th>Name</th>
+                           <th>Designation</th>
+                                </tr>
+                        </thead>
+                        <tbody>
+                         {employees.map(employee => {
+                         return (<tr key={employee.empId}>
+                          <td>{employee.empId}</td>
+                         <td>{employee.name}</td>
+                     <td>{employee.designation}</td>
+                                                </tr>)
+                                            })
+            }
+                        </tbody>
+                </table><br/><br/>
+                <button onClick={addEmployee} className="btn btn-primary">Add an Employee</button>
+        </>)
+    }
+    export default Employee;
+    
--- src/Employees.js
+++ src/Employees.js
@@ -1,12 +1,18 @@
-import { useState} from 'react';
-import Employee from './Employee';
+import { useState } from "react";
+import Employee from "./Employee";
 const Employees = () => {
-        const [employees] = useState([
- { empId: 1234, name: 'John', designation: 'SE' },
- { empId: 4567, name: 'Tom', designation: 'SSE' },
- { empId: 8910, name: 'Kevin', designation: 'TA' }])
-        return (
-            <Employee employees={employees}/>
-       )
-}
+  const [employees, setEmployees] = useState([
+    { empId: 1234, name: "John", designation: "SE" },
+    { empId: 4567, name: "Tom", designation: "SSE" },
+    { empId: 8910, name: "Kevin", designation: "TA" },
+  ]);
+  const addEmployee = () => {
+    setEmployees([
+      ...employees,
+      { empId: 6789, name: "Clara", designation: "TL" },
+    ]);
+  };
+  return <Employee employees={employees}  addEmployee={addEmployee} />;
+};
 export default Employees;
+
