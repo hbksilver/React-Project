769dc33e558b8c08eef200330aa24581899a9d48
hbksilver <37729528+hbksilver@users.noreply.github.com>
2024-01-26 15:34:13 -0600

    Understand how to make asynchronous calls within useEffect method.

--- src/Employee.js
+++ src/Employee.js
@@ -1,26 +1,40 @@
-const Employee = ({employees,addEmployee}) => {
-        return (<>
-                <table style={{ width: '60%' }} className='table'>
-                        <thead>
-                                <tr>
-                          <th>EmpID</th>
-                           <th>Name</th>
-                           <th>Designation</th>
-                                </tr>
-                        </thead>
-                        <tbody>
-                         {employees.map(employee => {
-                         return (<tr key={employee.empId}>
-                          <td>{employee.empId}</td>
-                         <td>{employee.name}</td>
-                     <td>{employee.designation}</td>
-                                                </tr>)
-                                            })
-            }
-                        </tbody>
-                </table><br/><br/>
-                <button onClick={addEmployee} className="btn btn-primary">Add an Employee</button>
-        </>)
-    }
-    export default Employee;
-    
+import { useState, useEffect } from "react";
+import axios from "axios";
+const Employees = () => {
+  const [employees, setEmployees] = useState([]);
+  const addEmployee = () => {
+    setEmployees([
+      ...employees,
+      { empId: 6789, name: "Clara", designation: "TL" },
+    ]);
+  };
+  useEffect(() => {
+    axios.get("http://localhost:4500/employee").then((result) => setEmployees(result.data));
+  }, []);
+  return (
+    <>
+      <table style={{ width: "60%" }} className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>EmpID</th>
+            <th>Name</th>
+            <th>Designation</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => {
+            return (
+              <tr>
+                <td>{employee.empId}</td>
+                <td>{employee.name}</td>
+                <td>{employee.designation}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button onClick={addEmployee}>Add an Employee</button>
+    </>
+  );
+};
+export default Employees;
