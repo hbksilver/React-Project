f27e6a19357d267645ade79fdb10904858de5747
hbksilver <37729528+hbksilver@users.noreply.github.com>
2024-01-30 15:58:17 -0600

    Redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Login from './components/Login';
-import Landing from './components/Landing';
-import NotFound from './components/NotFound';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>} />
-      <Route path="/landing/:name/*" element={<Landing/>}/>
-      <Route path="*" element={<NotFound/>} />
-    </Routes>
-  </BrowserRouter>
-);
-reportWebVitals();
+import reducer from './reducers/Reducer';
+var store = createStore(reducer)
