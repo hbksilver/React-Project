6f511fdac7dfa8abd14f96ab54a505e057ef77f1
hbksilver <37729528+hbksilver@users.noreply.github.com>
2024-01-30 10:44:44 -0600

    Create NotFound page and Configuring Routes Programmatically and Pass route parameters

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-import ContactUs from './components/ContactUs';
+import Login from './components/Login';
+import Landing from './components/Landing';
+import NotFound from './components/NotFound';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Header />}>
-      <Route index element={<Home />}/>
-      <Route path="home" element={<Home />}/>
-      <Route path="about" element={<About />}/>
-      <Route path="contact" element={<ContactUs />}/>
-    </Route>
-  </Routes>
-</BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>} />
+      <Route path="/landing/:name/*" element={<Landing/>}/>
+      <Route path="*" element={<NotFound/>} />
+    </Routes>
+  </BrowserRouter>
 );
 reportWebVitals();
